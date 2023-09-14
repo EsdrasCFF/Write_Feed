5e28fd6c4d21b71fac71bbdd43970391ab0a0a32
EsdrasCFF <ecff.dev@gmail.com>
2023-09-13 21:09:31 -0300

    Feat: Post Component Finished

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -18,9 +18,25 @@ export function Post() {
       <div className={styles.content} >
         <p>Fala galeraa 👋</p>
         <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p>jane.design/doctorcare</p>
-        <p> <a>#novoprojeto #nlw #rocketseat</a></p>
+        <p>{"  "}<a>jane.design/doctorcare</a></p>
+        <p> 
+          <a>#novoprojeto</a>{"  "}
+          <a>#nlw</a> {"  "}
+          <a>#rocketseat</a>
+        </p>
       </div>
+
+      <form className={styles.commentForm} >
+        <strong>Deixe seu feedback</strong>
+
+        <textarea 
+          placeholder='Deixe um comentário'
+        
+        />
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
